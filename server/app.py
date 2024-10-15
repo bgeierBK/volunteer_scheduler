ec1cd01e6f06ee1893c79b0a0c1e9c024c85e59b
bmgeier <geier.benjamin@gmail.com>
2024-10-15 16:53:52 -0400

    add user and school routes

--- server/app.py
+++ server/app.py
@@ -31,12 +31,11 @@ def create_user():
             fullName = request.json.get('username'),
             email = request.json.get('email'),
         )
-        new_user.hashed_password = bcrypt.generate_password_hash(request.json.get('password')).decode('utf-8')
+        new_user.hashed_password=request.json.get('password')
         db.session.add(new_user)
         db.session.commit()
         session['user_id'] = new_user.id
         return new_user.to_dict(), 201
-
     except Exception as e:
         return {'error': str(e)}, 406
 
@@ -65,4 +64,80 @@ def logout():
     session.pop('user_id', None)
     return {}, 204
 
+## user routes
 
+@app.get('/api/users')
+def get_users():
+    return [user.to_dict() for user in User.query.all()], 200
+
+@app.get('/api/users/<int:id>')
+def get_one_user(id):
+    user = User.query.get(id)
+    if user:
+        return jsonify(user.to_dict()), 200
+    return {}, 404
+
+@app.patch('/api/users/<int:id>')
+def update_user(id):
+    user = User.query.filter_by(id=id).first()
+    if user:
+        for key, value in request.json.items():
+            setattr(user, key, value)
+        db.session.commit()
+        return {}, 204
+    return {}, 404
+
+@app.delete('/api/users/<int:id>')
+def delete_user(id):
+    user = User.query.filter_by(id=id).first()
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return {}, 204
+    return {}, 404
+
+## school routes
+
+@app.get('/api/schools')
+def get_schools():
+    return [school.to_dict() for school in School.query.all()], 200
+
+@app.get('/api/schools/<int:id>')
+def get_one_school(id):
+    school = School.query.where(School.id == id).first()
+    if school:
+        return school.to_dict(), 200
+    return {}, 400
+
+@app.patch('/api/schools/<int:id>')
+def update_school(id):
+    school = School.query.where(School.id == id).first()
+    if school:
+        for key in request.json.keys():
+            setattr(school, key, request.json[key])
+            db.session.add(school)
+            db.sessoin.commit()
+            return school.to_dict()
+        return {}, 404
+
+@app.delete('/api/schools/<int:id>')
+def delete_school(id):
+    school = School.query.where(School.id ==id).first()
+    if school:
+        db.session.delete(school)
+        db.session.commit()
+        return {}, 204
+    return {}, 404
+
+@app.post('/api/schools')
+def add_school():
+    try:
+        new_school = School(
+            name = request.json.get('name'),
+        )
+        new_school.hashed_accessCode=request.json.get('code')
+        db.session.add(new_school)
+        db.session.commit()
+        return new_school.to_dict(), 201
+    except Exception as e:
+        return {'error': str(e)}, 406
